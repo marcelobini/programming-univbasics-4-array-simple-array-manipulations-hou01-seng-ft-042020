bf57a98ac208542587129a8e26e95184bc4b5853
Marcelo Bini <marcelobini@gmail.com>
2020-03-25 17:26:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,7 +7,6 @@ end
 def using_pop(array)
   array.pop
 end
-def pop_with_args(dog_breeds)
-  dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+def pop_with_args(array)
    array.pop[3, 4]
 end
